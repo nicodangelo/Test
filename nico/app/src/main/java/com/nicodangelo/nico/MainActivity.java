c7cc07db0e6bcd14a51a56222aabb655acccd6da
Jake Cox <nicodangelo15@gmail.com>
2015-03-07 21:58:49 -0600

    I finished up messing with a java made layout... This will become blank later so this is an importaint save:)

--- nico/app/src/main/java/com/nicodangelo/nico/MainActivity.java
+++ nico/app/src/main/java/com/nicodangelo/nico/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.*;
 import android.graphics.Color;
+import android.content.res.Resources;
+import android.util.*;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -18,22 +20,48 @@ public class MainActivity extends ActionBarActivity {
         RelativeLayout nicoLayout = new RelativeLayout(this);
         nicoLayout.setBackgroundColor(Color.CYAN);
 
+        //Button
+        Button redButton = new Button(this);
+        redButton.setText("Log In");
+        redButton.setBackgroundColor(Color.RED);
+
+        //UserName input
+        EditText username = new EditText(this);
+
+        //set the ID'S
+        redButton.setId(1);
+        username.setId(2);
+
+
         //the container for the button and other widgets
         RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
                 RelativeLayout.LayoutParams.WRAP_CONTENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT
         );
+        //container for the username field
+        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        //Give rules to position widgets
+        usernameDetails.addRule(RelativeLayout.ABOVE,redButton.getId());
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        usernameDetails.setMargins(0,0,0,50);
 
         buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
         buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
 
-        //Button
-        Button redButton = new Button(this);
-        redButton.setText("Click Me");
-        redButton.setBackgroundColor(Color.RED);
+        //gets the information about your app...
+        Resources r = getResources();
+        int px = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,200,r.getDisplayMetrics()); //this whole method will convert DIP to pixels for any size phone...
+        //then you pass that int value into the width (will change from device to device)
+        username.setWidth(px);
+
 
         //add the widget to layout(Button is now child of layout)
         nicoLayout.addView(redButton,buttonDetails);
+        nicoLayout.addView(username,usernameDetails);
 
         //Set this activities content/display to this view
         setContentView(nicoLayout);
